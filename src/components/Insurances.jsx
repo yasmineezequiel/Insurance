94d584179fa38880286d81bde94a300ab79c21f8
yasmineezequiel <yasmine.ezequiel@gmail.com>
2021-10-28 21:55:57 +0200

    Task 2: Implement UI.

--- src/components/Insurances.jsx
+++ src/components/Insurances.jsx
@@ -1,9 +1,16 @@
 import { useEffect, useState } from "react";
+// Move all Swiper into a new UI component
+import { Swiper, SwiperSlide } from "swiper/react";
+import "./Insurances.css";
+import "swiper/swiper-bundle.css";
+import SwiperCore, { Pagination, Navigation } from "swiper";
+SwiperCore.use([Pagination, Navigation]);
 
 const Insurances = () => {
   const [loading, setLoading] = useState(false);
   const [insuranceOptions, setInsuranceOptions] = useState([]);
 
+  // Add a new hook for fetch api if time allows
   const fetchData = async () => {
     try {
       const response = await fetch(
@@ -26,25 +33,67 @@ const Insurances = () => {
   return (
     <>
       {loading === true && <div>Loading</div>}
-      <h1
-        style={{ fontFamily: "sans-serif", fontSize: "18px", color: "#121212" }}
-      >
-        {"Our Coverage".toUpperCase()}
-      </h1>
-      {insuranceOptions.map((insurance, index) => (
-        <>
-          <div key={index}>
-            <div className="icon">
-              <img
-                src={insurance.icon.variants.light.svgUrl}
-                alt="Icon"
-                style={{ width: "40px", height: "40px" }}
-              />
-            </div>
-            <div>{insurance.title}</div>
-          </div>
-        </>
-      ))}
+      <div className="app" style={{ paddingTop: "10%" }}>
+        <h1
+          style={{
+            fontFamily: "sans-serif",
+            fontSize: "18px",
+            color: "#121212",
+            textAlign: "center",
+            paddingBottom: "5%",
+          }}
+        >
+          {"Our Coverage".toUpperCase()}
+        </h1>
+        <Swiper
+          slidesPerView={1}
+          spaceBetween={1}
+          navigation
+          pagination={{ clickable: true }}
+          breakpoints={{
+            "@0.00": {
+              slidesPerView: 1,
+              spaceBetween: 9,
+            },
+            "@0.75": {
+              slidesPerView: 1,
+              spaceBetween: 25,
+            },
+            "@1.00": {
+              slidesPerView: 3,
+              spaceBetween: 40,
+            },
+            "@1.50": {
+              slidesPerView: 4,
+              spaceBetween: 50,
+            },
+          }}
+          className="mySwiper"
+        >
+          {insuranceOptions.map((insurance, index) => (
+            <>
+              <SwiperSlide key={index}>
+                <div className="icon">
+                  <img
+                    src={insurance.icon.variants.light.svgUrl}
+                    alt="Icon"
+                    style={{ width: "40px", height: "40px" }}
+                  />
+                  <div
+                    style={{
+                      margin: "5px",
+                      paddingTop: "20px",
+                      textAlign: "left",
+                    }}
+                  >
+                    {insurance.title}
+                  </div>
+                </div>
+              </SwiperSlide>
+            </>
+          ))}
+        </Swiper>
+      </div>
     </>
   );
 };
